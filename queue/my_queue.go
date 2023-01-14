346f1604db45f0c3fbe90edb0f9e49f8108e8d16
liuzhe7 <zhixizhixizhixi@gmail.com>
2023-01-14 20:07:34 +0800

    stack queue

--- queue/my_queue.go
+++ queue/my_queue.go
@@ -16,6 +16,12 @@ type MyQueue struct {
 func Constructor() MyQueue {
 	return MyQueue{}
 }
+func (this *MyQueue) inToOut() {
+	for i := len(this.inStack) - 1; i >= 0; i-- {
+		this.outStack = append(this.outStack, this.inStack[i])
+	}
+	this.inStack = this.inStack[:0]
+}
 
 func (this *MyQueue) Push(x int) {
 	this.inStack = append(this.inStack, x)
@@ -23,10 +29,7 @@ func (this *MyQueue) Push(x int) {
 
 func (this *MyQueue) Pop() int {
 	if len(this.outStack) == 0 {
-		for i := len(this.inStack) - 1; i >= 0; i++ {
-			this.outStack = append(this.outStack, this.inStack[i])
-			this.inStack = this.inStack[:0]
-		}
+		this.inToOut()
 	}
 	popInt := this.outStack[len(this.outStack)-1]
 	this.outStack = this.outStack[:len(this.outStack)-1]
@@ -35,10 +38,7 @@ func (this *MyQueue) Pop() int {
 
 func (this *MyQueue) Peek() int {
 	if len(this.outStack) == 0 {
-		for i := len(this.inStack) - 1; i >= 0; i++ {
-			this.outStack = append(this.outStack, this.inStack[i])
-			this.inStack = this.inStack[:0]
-		}
+		this.inToOut()
 	}
 	return this.outStack[len(this.outStack)-1]
 }
