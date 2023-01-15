0ebf253e24e1a20e54284ce0f58103b2e6235018
liuzhe7 <zhixizhixizhixi@gmail.com>
2023-01-15 18:19:27 +0800

    fib

---
+++ recursion/fib.go
@@ -0,0 +1,28 @@
+/*
+Author : Liu Zhe
+Date   : 2023/1/15
+*/
+
+/**********************************************************************************
+
+**********************************************************************************/
+
+package recursion
+
+// f(n) = f(n-1) + f(n-2)
+
+// f(0) return 0
+// f(1) return 1
+var record = map[int]int{}
+
+func fib(n int) int {
+	if n == 1 || n == 0 {
+		return n
+	}
+	score, ok := record[n]
+	if ok {
+		return score
+	}
+	record[n] = fib(n-1) + fib(n-2)
+	return record[n]
+}
