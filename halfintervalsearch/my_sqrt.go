432492f1933f69e8cdd0a3f183f082aec0d08dee
liuzhe7 <zhixizhixizhixi@gmail.com>
2023-01-17 13:01:44 +0800

    my_sqrt.go

---
+++ halfintervalsearch/my_sqrt.go
@@ -0,0 +1,28 @@
+/*
+Author : Liu Zhe
+Date   : 2023/1/17
+*/
+
+/**********************************************************************************
+
+**********************************************************************************/
+
+package halfintervalsearch
+
+func mySqrt(x int) int {
+	min := 0
+	max := x
+	for min <= max {
+		mid := min + (max-min)>>1
+		if mid*mid == x {
+			return mid
+		}
+		if mid*mid < x {
+			min = mid + 1
+		}
+		if mid*mid > x {
+			max = mid - 1
+		}
+	}
+	return min - 1
+}
