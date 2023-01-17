b7c7849ff92a4172ac8514d8b02b901ad9479bcd
liuzhe7 <zhixizhixizhixi@gmail.com>
2023-01-17 12:42:36 +0800

    search_insert.go

---
+++ halfintervalsearch/search_insert.go
@@ -0,0 +1,28 @@
+/*
+Author : Liu Zhe
+Date   : 2023/1/17
+*/
+
+/**********************************************************************************
+
+**********************************************************************************/
+
+package halfintervalsearch
+
+func searchInsert(nums []int, target int) int {
+	max := len(nums) - 1
+	min := 0
+	for min <= max {
+		mid := min + (max-min)>>1
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] > target {
+			max = mid - 1
+		}
+		if nums[mid] < target {
+			min = mid + 1
+		}
+	}
+	return max + 1
+}
