1722853c3dc28d6812ec0639e0b201a082b569f0
liuzhe7 <zhixizhixizhixi@gmail.com>
2023-01-18 09:10:39 +0800

    is_same_tree.go

---
+++ tree/is_same_tree.go
@@ -0,0 +1,25 @@
+/*
+Author : Liu Zhe
+Date   : 2023/1/18
+*/
+
+/**********************************************************************************
+https://leetcode.cn/problems/same-tree/submissions/
+**********************************************************************************/
+
+package tree
+
+// f(p,q) = f(p.left,q.left) && f(p.right,q.right) && p.val==q.val
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil && q != nil {
+		return false
+	}
+	if p != nil && q == nil {
+		return false
+	}
+
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right) && p.Val == q.Val
+}
