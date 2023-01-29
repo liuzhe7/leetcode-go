61d23d0650f3626ab680fa7bda7de3115e5a5591
liuzhe7 <zhixizhixizhixi@gmail.com>
2023-01-29 10:19:15 +0800

    ispalindrome

---
+++ number/is_palindrome.go
@@ -0,0 +1,27 @@
+/*
+Author : Liu Zhe
+Date   : 2023/1/29
+*/
+
+/**********************************************************************************
+https://leetcode.cn/problems/palindrome-number/submissions/
+**********************************************************************************/
+
+package number
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+	var reverseInt int
+	var xCopy = x
+	for {
+		lastNumber := x % 10
+		reverseInt = reverseInt*10 + lastNumber
+		if x < 10 {
+			break
+		}
+		x = x / 10
+	}
+	return reverseInt == xCopy
+}
